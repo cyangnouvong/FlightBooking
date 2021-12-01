3565772d7e7e7455ea2abbca7fd207dd43c24a8c
r-tafete <60676888+r-tafete@users.noreply.github.com>
2021-11-30 23:45:04 -0500

    Started phase III - Worked on views
    Got through all but view_owners and view_customers (there's an answer for view_owners but it's wrong), not confident in any of them though lol

---
+++ cs4400_phase3_stored_procedures_template_v1.sql
@@ -0,0 +1,395 @@
+-- CS4400: Introduction to Database Systems (Fall 2021)
+-- Phase III: Stored Procedures & Views [v0] Tuesday, November 9, 2021 @ 12:00am EDT
+-- Team 32
+-- Rebecca Tafete (rtafete3)
+-- Serena Gao (sgao97)
+-- Chelsea Yangnouvong (cyangnouvong3)
+-- Keely Culbertson (kculbertson3)
+-- Directions:
+-- Please follow all instructions for Phase III as listed on Canvas.
+-- Fill in the team number and names and GT usernames for all members above.
+
+
+-- ID: 1a
+-- Name: register_customer
+drop procedure if exists register_customer;
+delimiter //
+create procedure register_customer (
+    in i_email varchar(50),
+    in i_first_name varchar(100),
+    in i_last_name varchar(100),
+    in i_password varchar(50),
+    in i_phone_number char(12),
+    in i_cc_number varchar(19),
+    in i_cvv char(3),
+    in i_exp_date date,
+    in i_location varchar(50)
+) 
+sp_main: begin
+-- TODO: Implement your solution here
+-- email and phone number must be unique
+-- credit card number must be unique
+-- phone number must be unique
+-- if customer already exists as an account/client but not customer, add them as customer to indicate they are both an owner and a customer
+ 
+
+
+end //
+delimiter ;
+
+
+-- ID: 1b
+-- Name: register_owner
+drop procedure if exists register_owner;
+delimiter //
+create procedure register_owner (
+    in i_email varchar(50),
+    in i_first_name varchar(100),
+    in i_last_name varchar(100),
+    in i_password varchar(50),
+    in i_phone_number char(12)
+) 
+sp_main: begin
+-- TODO: Implement your solution here
+
+end //
+delimiter ;
+
+
+-- ID: 1c
+-- Name: remove_owner
+drop procedure if exists remove_owner;
+delimiter //
+create procedure remove_owner ( 
+    in i_owner_email varchar(50)
+)
+sp_main: begin
+-- TODO: Implement your solution here
+
+end //
+delimiter ;
+
+
+-- ID: 2a
+-- Name: schedule_flight
+drop procedure if exists schedule_flight;
+delimiter //
+create procedure schedule_flight (
+    in i_flight_num char(5),
+    in i_airline_name varchar(50),
+    in i_from_airport char(3),
+    in i_to_airport char(3),
+    in i_departure_time time,
+    in i_arrival_time time,
+    in i_flight_date date,
+    in i_cost decimal(6, 2),
+    in i_capacity int,
+    in i_current_date date
+)
+sp_main: begin
+-- TODO: Implement your solution here
+
+end //
+delimiter ;
+
+
+-- ID: 2b
+-- Name: remove_flight
+drop procedure if exists remove_flight;
+delimiter //
+create procedure remove_flight ( 
+    in i_flight_num char(5),
+    in i_airline_name varchar(50),
+    in i_current_date date
+) 
+sp_main: begin
+-- TODO: Implement your solution here
+
+end //
+delimiter ;
+
+
+-- ID: 3a
+-- Name: book_flight
+drop procedure if exists book_flight;
+delimiter //
+create procedure book_flight (
+    in i_customer_email varchar(50),
+    in i_flight_num char(5),
+    in i_airline_name varchar(50),
+    in i_num_seats int,
+    in i_current_date date
+)
+sp_main: begin
+-- TODO: Implement your solution here
+
+end //
+delimiter ;
+
+-- ID: 3b
+-- Name: cancel_flight_booking
+drop procedure if exists cancel_flight_booking;
+delimiter //
+create procedure cancel_flight_booking ( 
+    in i_customer_email varchar(50),
+    in i_flight_num char(5),
+    in i_airline_name varchar(50),
+    in i_current_date date
+)
+sp_main: begin
+-- TODO: Implement your solution here
+
+end //
+delimiter ;
+
+-- ID: 3c
+-- Name: view_flight
+create or replace view view_flight (
+    flight_id,
+    flight_date,
+    airline,
+    destination,
+    seat_cost,
+    num_empty_seats,
+    total_spent
+) as
+select flight.Flight_Num, Flight_Date, flight.Airline_Name, To_Airport, Cost, Capacity - sum(Num_Seats), sum(Cost + (Cost * 0.2 * Was_Cancelled))
+	from flight natural join book;
+    
+
+-- ID: 4a
+-- Name: add_property
+drop procedure if exists add_property;
+delimiter //
+create procedure add_property (
+    in i_property_name varchar(50),
+    in i_owner_email varchar(50),
+    in i_description varchar(500),
+    in i_capacity int,
+    in i_cost decimal(6, 2),
+    in i_street varchar(50),
+    in i_city varchar(50),
+    in i_state char(2),
+    in i_zip char(5),
+    in i_nearest_airport_id char(3),
+    in i_dist_to_airport int
+) 
+sp_main: begin
+-- TODO: Implement your solution here
+  
+end //
+delimiter ;
+
+
+-- ID: 4b
+-- Name: remove_property
+drop procedure if exists remove_property;
+delimiter //
+create procedure remove_property (
+    in i_property_name varchar(50),
+    in i_owner_email varchar(50),
+    in i_current_date date
+)
+sp_main: begin
+-- TODO: Implement your solution here
+    
+end //
+delimiter ;
+
+
+-- ID: 5a
+-- Name: reserve_property
+drop procedure if exists reserve_property;
+delimiter //
+create procedure reserve_property (
+    in i_property_name varchar(50),
+    in i_owner_email varchar(50),
+    in i_customer_email varchar(50),
+    in i_start_date date,
+    in i_end_date date,
+    in i_num_guests int,
+    in i_current_date date
+)
+sp_main: begin
+-- TODO: Implement your solution here
+
+end //
+delimiter ;
+
+
+-- ID: 5b
+-- Name: cancel_property_reservation
+drop procedure if exists cancel_property_reservation;
+delimiter //
+create procedure cancel_property_reservation (
+    in i_property_name varchar(50),
+    in i_owner_email varchar(50),
+    in i_customer_email varchar(50),
+    in i_current_date date
+)
+sp_main: begin
+-- TODO: Implement your solution here
+
+end //
+delimiter ;
+
+
+-- ID: 5c
+-- Name: customer_review_property
+drop procedure if exists customer_review_property;
+delimiter //
+create procedure customer_review_property (
+    in i_property_name varchar(50),
+    in i_owner_email varchar(50),
+    in i_customer_email varchar(50),
+    in i_content varchar(500),
+    in i_score int,
+    in i_current_date date
+)
+sp_main: begin
+-- TODO: Implement your solution here
+    
+end //
+delimiter ;
+
+
+-- ID: 5d
+-- Name: view_properties
+create or replace view view_properties (
+    property_name, 
+    average_rating_score, 
+    description, 
+    address, 
+    capacity, 
+    cost_per_night
+) as
+select Property_Name, avg(customers_rate_owners.Score), Descr, concat(Street, ", ", City, ", ", State, ", ", Zip), Capacity, Cost
+    from property natural join customers_rate_owners;
+
+-- ID: 5e
+-- Name: view_individual_property_reservations
+drop procedure if exists view_individual_property_reservations;
+delimiter //
+create procedure view_individual_property_reservations (
+    in i_property_name varchar(50),
+    in i_owner_email varchar(50)
+)
+sp_main: begin
+    drop table if exists view_individual_property_reservations;
+    create table view_individual_property_reservations (
+        property_name varchar(50),
+        start_date date,
+        end_date date,
+        customer_email varchar(50),
+        customer_phone_num char(12),
+        total_booking_cost decimal(6,2),
+        rating_score int,
+        review varchar(500)
+    ) as
+    -- TODO: replace this select query with your solution
+    select 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8' from reserve;
+
+end //
+delimiter ;
+
+
+-- ID: 6a
+-- Name: customer_rates_owner
+drop procedure if exists customer_rates_owner;
+delimiter //
+create procedure customer_rates_owner (
+    in i_customer_email varchar(50),
+    in i_owner_email varchar(50),
+    in i_score int,
+    in i_current_date date
+)
+sp_main: begin
+-- TODO: Implement your solution here
+
+end //
+delimiter ;
+
+
+-- ID: 6b
+-- Name: owner_rates_customer
+drop procedure if exists owner_rates_customer;
+delimiter //
+create procedure owner_rates_customer (
+    in i_owner_email varchar(50),
+    in i_customer_email varchar(50),
+    in i_score int,
+    in i_current_date date
+)
+sp_main: begin
+-- TODO: Implement your solution here
+
+end //
+delimiter ;
+
+
+-- NOT AT ALL confident with this one lol
+-- ID: 7a
+-- Name: view_airports
+create or replace view view_airports (
+    airport_id, 
+    airport_name, 
+    time_zone, 
+    total_arriving_flights, 
+    total_departing_flights, 
+    avg_departing_flight_cost
+) as
+select Airport_Id, Airport_Name, Time_Zone, count(To_Airport), count(From_Airport), avg(Cost) from airport join flight 
+	where airport.Airport_Id = flight.To_Airport 
+		or airport.Airport_Id = flight.From_Airport;
+
+
+-- ID: 7b
+-- Name: view_airlines
+create or replace view view_airlines (
+    airline_name, 
+    rating, 
+    total_flights, 
+    min_flight_cost
+) as
+select Airline_Name, Rating, count(*), min(Cost) from airline natural join flight;
+
+
+-- ID: 8a
+-- Name: view_customers
+create or replace view view_customers (
+    customer_name, 
+    avg_rating, 
+    location, 
+    is_owner, 
+    total_seats_purchased
+) as
+-- TODO: replace this select query with your solution
+-- view customers
+select 'col1', 'col2', 'col3', 'col4', 'col5' from customer;
+
+-- THIS ONE IS WRONG LOL but if you figure this one out then you've basically figured out view_customers so
+-- ID: 8b
+-- Name: view_owners
+create or replace view view_owners (
+    owner_name, 
+    avg_rating, 
+    num_properties_owned, 
+    avg_property_rating
+) as
+select concat(First_Name, " ", Last_Name), avg(Score) from accounts join customers_rate_owners where accounts.Email = customers_rate_owners.Owner_Email
+	union
+select count(Property_Name), avg(Score) from property natural join review;
+    
+
+-- ID: 9a
+-- Name: process_date
+drop procedure if exists process_date;
+delimiter //
+create procedure process_date ( 
+    in i_current_date date
+)
+sp_main: begin
+-- TODO: Implement your solution here
+    
+end //
+delimiter ;
